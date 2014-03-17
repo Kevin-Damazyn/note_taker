44a983cb457fb614aaa5038962f8869b386b913e
dlawre14 <dlawre14@slu.edu>
2014-03-17 12:23:15 -0500

    baseline work for Speech to Text (STT) testing enviornment
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	new file:   .gitignore
    #	new file:   sprint2.txt
    #

---
+++ NoteTaker/src/main/java/com/recording/STTActivity.java
@@ -0,0 +1,19 @@
+package com.recording;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.notetaker.R;
+
+/**
+ * Created by Dylan on 3/17/14.
+ */
+public class STTActivity extends Activity {
+
+    public void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
+
+        setContentView(R.layout.stt_layout);
+    }
+
+}
