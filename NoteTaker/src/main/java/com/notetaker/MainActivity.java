587040efff4afc77e1dc8e4fb6597f3f80403643
Kevin-Damazyn <damazynk@slu.edu>
2014-04-25 15:07:25 -0500

    Fixes 4/25/14

--- NoteTaker/src/main/java/com/notetaker/MainActivity.java
+++ NoteTaker/src/main/java/com/notetaker/MainActivity.java
@@ -3,6 +3,7 @@ package com.notetaker;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,6 +20,7 @@ import com.recording.RecordActivity;
 import com.recording.STTActivity;
 import com.transcription.SphinxTranscriptionService;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -116,5 +118,8 @@ public class MainActivity extends Activity {
         startService(i);
     }
 
+    public static File getDirect(){
+        return direct;
+    }
 
 }
